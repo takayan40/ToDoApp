5dbc320c9badc6b3f798ee52de7e6c78ddf386f9
takayan <f15bc010@gmail.com>
2020-02-20 19:36:15 +0900

    ignoreの変更

--- app/src/main/java/io/github/takayan40/todoapp/MainActivity.kt
+++ app/src/main/java/io/github/takayan40/todoapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.github.takayan40.todoapp
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -8,13 +9,17 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toolbar
 import io.github.takayan40.todoapp.ui.main.SectionsPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar.title = "一覧"
+
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
@@ -23,8 +28,8 @@ class MainActivity : AppCompatActivity() {
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            val intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
         }
     }
 }
